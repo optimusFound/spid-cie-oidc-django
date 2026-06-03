52da1f925a3d4e84550d71637b90bb3beac85ca0
Luca Sordi <luca.sordi@mynet.it>
2026-06-03 14:39:31 +0200

    ssl req False ONLY for test

--- spid_cie_oidc/entity/settings.py
+++ spid_cie_oidc/entity/settings.py
@@ -56,7 +56,7 @@ HTTPC_PARAMS = getattr(
     settings,
     "HTTPC_PARAMS",
     {
-        "connection": {"ssl": True},
+        "connection": {"ssl": False}, #TODO Rimuove, solo per test
         "session": {"timeout": aiohttp.ClientTimeout(total=HTTPC_TIMEOUT)},
     },
 )
